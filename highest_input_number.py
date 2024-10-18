058165f686f7634fa7f88eec50c1a15ace87ee06
Adrian Ranel <adrianranelm@gmail.com>
2024-10-18 22:06:25 +0800

    comparing_complete

--- highest_input_number.py
+++ highest_input_number.py
@@ -1,4 +1,18 @@
 #define function to compare
-#user input 5 numbers
+def compare(int1, int2, int3, int4, int5):
+    if int1 >= int2:
+        highest = int1
+    else:
+        highest = int2
+
+    if int3 > highest:
+        highest = int3
+
+    if int4 > highest:
+        ighest = int4
+
+    if int5 > highest:
+        highest = int5
+#user input 5 numberds
 #use define function
 #output largest number
